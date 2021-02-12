e4ae1de1f65cd109452290050474a213b8167507
frgr2254 <fredrikgrote.gmail.com>
2021-02-12 13:16:48 +0100

    examples directory removed

--- examples/write_output.py
+++
@@ -1,9 +0,0 @@
-#This Python module contain functions for writing output files
-
-def write_output(atom_types,elements):
-    "This function write the assigned atom types to file"
-    outfile = open('output.out','w')
-    for i in range(len(atom_types)):
-        outfile.write(elements[i]+' '+atom_types[i]+'\n')
-    outfile.close()
-    
